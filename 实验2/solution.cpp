15a9326083d0a356eb154620313539497d5fdf16
DarkComLikeEating <87956422+DarkComLikeEating@users.noreply.github.com>
2023-03-30 21:03:33 +0800

    实验2小更新
    solution里添加了一些说明信息。

--- 实验2/solution.cpp
+++ 实验2/solution.cpp
@@ -7,10 +7,11 @@ void solution() {
 	OneVarPoly * P1, * P2, * ans;
 	cout << "在下方输入第一个一元多项式." << endl;
 	CreatePolyR(P1);
+	cout << "输入的第一个多项式："; DispPoly(P1);
 	cout << endl;
 	cout << "在下方输入第二个一元多项式." << endl;
 	CreatePolyR(P2);
-
+	cout << "输入的第二个多项式："; DispPoly(P2);
 	cout << endl;
 	ans = Add(P1, P2);
 	cout << "两个多项式之和为：" << endl;
